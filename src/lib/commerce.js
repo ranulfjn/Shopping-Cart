6234411332ded60e717d22b3177d9517986a586a
ranulfjn <ranulfnoronha@gmail.com>
2021-02-13 22:15:24 -0500

    cart update

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,3 @@
 import Commerce from '@chec/commerce.js';
-import Commece from '@chec/commerce.js';
 
-export const commerce = new Commerce('pk_232883f0a9ff77ee762931c52c84944114c751c49ae04', true);
+export const commerce = new Commerce('', true);
