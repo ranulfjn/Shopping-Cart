34d74b178d699049ecb3529deb994130a52c4210
ranulfjn <ranulfnoronha@gmail.com>
2021-02-28 16:28:23 -0500

    paymentForm

---
+++ src/components/CheckoutForm/Review.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+
+const Review = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Review
